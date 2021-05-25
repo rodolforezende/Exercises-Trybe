91e9d8a68e423efc65d3447f1aac8f8cf91119b8
Rodolfo <rodolforrcont@gmail.com>
2021-05-25 13:34:56 -0300

    Objetos e For/In

---
+++ bloco.4/exercises/introducao-a-javascript-e-logica-de-programacao_4/script.js
@@ -0,0 +1,352 @@
+//Exercício Fixação - 1
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { golden: 2, silver: 3 },
+};
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
+
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+//Exercício 1
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+//Duas formas de resolver o exercício
+console.log('Bem-vinda, '+ info.personagem);
+
+console.log('Bem-vinda, '+ info['personagem']);
+
+//Exercício 2
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+
+//Duas formas de resolver o exercício
+info.recorrente = 'Sim';
+
+info['recorrente'] = 'Sim';
+
+console.log(info);
+
+//Exercício 3
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  };
+  
+for(let properties in info){
+    console.log(properties);
+ }
+
+ //Exercício 4
+ let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  };
+  
+  for(let properties in info){
+    console.log(info[properties]);
+  }
+
+// Exercício 5
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  };
+  
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+  
+  for (let properties in info) {
+    if (
+      info[properties] === info.recorrente &&
+      info[properties] === 'Sim' &&
+      info2[properties] === 'Sim'
+    ) {
+      console.log('Ambos recorrentes');
+    } else {
+      console.log(info[properties] + ' e ' + info2[properties]);
+    }
+  }
+
+//Parte II
+//Exercício 1
+//Duas formas de resolver o exercício
+function verificaPalindrome(palavra) {
+    let arrayLetras = palavra.split('');
+    let isPalindrome = true;
+    for (let index in arrayLetras) {
+      if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+        isPalindrome = false;
+      }
+    }
+    return isPalindrome;
+  }
+  
+  function verificaPalindrome(string) {
+    let reverse = string.split('').reverse().join('');
+    if (reverse === string) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  console.log(verificaPalindrome('arara')); //true
+  console.log(verificaPalindrome('desenvolvimento')); //false
+
+//Exercício 2
+
+function indiceDoMaior(numeros) {
+    let indiceMaior = 0;
+    for (let indice in numeros) {
+      if (numeros[indiceMaior] < numeros[indice]) {
+        indiceMaior = indice;
+      }
+    }
+    return indiceMaior;
+  }
+  
+  console.log(indiceDoMaior([2, 3, 6, 7, 10, 1])); //4
+
+//Exerício 3
+function indiceDoMaior(numeros) {
+    let indiceMaior = 0;
+    for (let indice in numeros) {
+      if (numeros[indiceMaior] < numeros[indice]) {
+        indiceMaior = indice;
+      }
+    }
+    return indiceMaior;
+  }
+  
+  console.log(indiceDoMaior([2, 3, 6, 7, 10, 1])); //4
+
+//Exercício 4
+function maiorPalavra(palavras) {
+    let maiorPalavra = palavras[0];
+    for (let indice in palavras) {
+      if (maiorPalavra.length < palavras[indice].length) {
+        maiorPalavra = palavras[indice];
+      }
+    }
+    return maiorPalavra;
+  }
+  
+  console.log(maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])); //Fernanda
+
+//Exercício 5
+//Duas formas de resolver o exercício
+function maisRepetido(numeros) {
+    let contRepetido = 0;
+    let contNumero = 0;
+    let indexNumeroRepetido = 0;
+    for (let index in numeros) {
+      let verificaNumero = numeros[index];
+      for (let index2 in numeros) {
+        if (verificaNumero === numeros[index2]) {
+          contNumero += 1;
+        }
+      }
+      if (contNumero > contRepetido) {
+        contRepetido = contNumero;
+        indexNumeroRepetido = index;
+      }
+      contNumero = 0;
+    }
+    return numeros[indexNumeroRepetido];
+  }
+  
+  function maisRepetido(numeros) {
+    let num = {};
+  
+    for (let index = 0; index < numeros.length; index += 1) {
+      let valor = numeros[index];
+      if (num[valor] === undefined) {
+        num[valor] = 1;
+      } else {
+        num[valor] = num[valor] + 1;
+      }
+    }
+  
+    let contRepetido = 0;
+    let contNumero = 0;
+  
+    for (let prop in num) {
+      if (contRepetido < num[prop]) {
+        contRepetido = num[prop];
+        contNumero = prop;
+      }
+    }
+  
+    return contNumero;
+  }
+  
+  console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3])); //2
+
+//Exercício 6
+function somaTodosNumeros(numeros) {
+    let total = 0;
+    for (let index = 1; index <= numeros; index += 1) {
+      total = total + index;
+    }
+    return total;
+  }
+  console.log(somaTodosNumeros(5)); //15
+
+//Exercício 7
+//Duas formas de resolver o exercício
+function verificaFimPalavra(palavra, fimPalavra) {
+    palavra = palavra.split('');
+    fimPalavra = fimPalavra.split('');
+    controle = true;
+    for (let index = 0; index < fimPalavra.length; index += 1) {
+      if (palavra[palavra.length - fimPalavra.length + index] != fimPalavra[index]) {
+        controle = false;
+      }
+    }
+    return controle;
+  }
+  
+  function verificaFimPalavra(palavra, fimPalavra) {
+    let inversoPalavra = palavra.split('').reverse().join('');
+    let inversoFimPalavra = fimPalavra.split('').reverse().join('');
+    let result;
+  
+    for (let index = 0; index < inversoFimPalavra.length; index += 1) {
+      if (inversoPalavra[index] !== inversoFimPalavra[index]) {
+        result = false;
+        break;
+      } else {
+        result = true;
+      }
+    }
+    
+    return result;
+  }
+  
+  console.log(verificaFimPalavra('trybe', 'be')); //true
+  console.log(verificaFimPalavra('joaofernando', 'fernan')); //false
+
+//Bônus
+
+
+function Romanos(roma) {
+    let romanos = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+  
+    let arrayNumbers = [];
+    let result = 0;
+  
+    for (let indice in roma) {
+      arrayNumbers[indice] = romanos[roma[indice]];
+    }
+  
+    for (let indice in arrayNumbers) {
+      if (arrayNumbers[indice] < arrayNumbers[indice + 1]) {
+        arrayNumbers[indice + 1] = arrayNumbers[indice + 1] - arrayNumbers[indice];
+      } else {
+        result += arrayNumbers[indice];
+      }
+    }
+  
+    return result;
+  }
+  
+  console.log(Romanos(MMXVIII));
+
+   
+const vector = [[1, 2], [3, 4, 5, 6], [7, 8, 9, 10]];
+
+function arrayOfNumbers(vector) {
+  const result = [];
+  for (let index = 0; index < vector.length; index += 1) {
+    const numbers = vector[index];
+    for (let indexSub = 0; indexSub < numbers.length; indexSub += 1) {
+      const current = numbers[indexSub];
+      if ((current % 2) === 0) {
+        result.push(current);
+      } 
+    }
+  }
+  return result;
+}
+
+console.log(arrayOfNumbers(vector));
+
+
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+  ];
+  
+  const result = {};
+  
+  for (let index = 0; index < basket.length; index += 1) {
+    const fruit = basket[index];
+    if (!result[fruit]) result[fruit] = 0;
+    result[fruit] += 1;
+  }
+  
+  const summaries = [];
+  for (fruit in result) {
+    let message = `${result[fruit]} ${fruit}`;
+    if (result[fruit] > 1) message += 's';
+    summaries.push(message);
+  }
+  
+  console.log(`Sua cesta possui: ${summaries.join(', ')}.`);
